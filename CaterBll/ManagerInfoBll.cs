62cc7bd7023449036b52eee4273e667c80ea5f88
德宏 姜 <wormtwo@outlook.com>
2018-05-29 19:43:41 +0800

    管理员界面的增删查改

---
+++ CaterBll/ManagerInfoBll.cs
@@ -0,0 +1,33 @@
+﻿using CaterDal;
+using CaterModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaterBll
+{
+    public partial class ManagerInfoBll
+    {
+        ManagerInfoDal miDal = new ManagerInfoDal();
+        public List<ManagerInfo> GetList()
+        {
+            return miDal.GetList();
+        }
+
+        public bool Add(ManagerInfo mi)
+        {
+            //调用dal层的insert方法
+            return miDal.Insert(mi) > 0;
+        }
+        public bool Edit(ManagerInfo mi)
+        {
+            return miDal.Update(mi) > 0;
+        }
+        public bool Remove(int id)
+        {
+            return miDal.Delete(id) > 0;
+        }
+    }
+}
---
+++ CaterCommon/Md5Helper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaterCommon
+{
+   public partial class Md5Helper
+    {
+        public static string EncryptString(string str)
+        {
+            //utf8,x2
+            //创建对象的方法:构造方法,静态方法(工厂模式)
+            MD5 md5 = MD5.Create();
+            //将字符串转换成字节数组
+            byte[] byteOld = Encoding.UTF8.GetBytes(str);
+            //调用加密方法
+            byte[] byteNew= md5.ComputeHash(byteOld);
+            //将加密结果进行转换字符串
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in byteNew)
+            {
+                //将字节转换橙16进制表示的字符串,,恒占2位
+                sb.Append(b.ToString("x2"));
+            }
+            //返回加密的字符串
+            return sb.ToString();
+        }
+    }
+}
